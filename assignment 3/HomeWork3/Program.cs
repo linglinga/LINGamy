f11ec55da86480033ada5568a82a8a80f642c5a9
linga <linga@uw.edu>
2016-10-16 21:32:05 -0700

    HomeWork3

---
+++ assignment 3/HomeWork3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork3 // HomeWork 3: Control Statements
+                    // take seconds and converts it into days, hours, minutes, seconds per minute.
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 1. ask user to input the seconds to be converted for the console.
+            Console.Write("Please input how many seconds: ");
+            
+
+            // 2. read the number from console, and store it into a string type.
+            string str; // declar the string. store the string
+            str = Console.ReadLine();   // read it and store it.
+
+            // 3. convert the number that is in the string format to the numeric format.
+            int x = int.Parse(str);
+            int seconds = x;
+
+            // 4. convert number to days, hours, minutes & seconds.
+            // variable that has the number seconds per day, the number seconds per hour, and the number seconds per minute.
+
+            TimeSpan t = TimeSpan.FromSeconds(seconds);
+
+            int d = t.Days;
+            int hr = t.Hours;
+            int mn = t.Minutes;
+            int ss = t.Seconds;
+
+            Console.WriteLine("Days:" + d + " HR:" + hr + " M:" + mn + " S:" + ss);
+
+            Console.ReadLine(); // to hold up the console, always leave this in here.
+
+        }
+    }
+}
